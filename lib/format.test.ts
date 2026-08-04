c2906cc73e9c832eea2d0de6e24b4c151118341f
Chrystalio <chrystalio.k@outlook.com>
2026-08-04 18:10:29 +0700

    test: add formatIDR millions case

--- lib/format.test.ts
+++ lib/format.test.ts
@@ -9,4 +9,8 @@ describe('formatIDR', () => {
   it('formats zero', () => {
     expect(formatIDR(0)).toBe('Rp 0');
   });
+
+  it('formats millions with multiple separators', () => {
+    expect(formatIDR(1500000)).toBe('Rp 1.500.000');
+  });
 });
